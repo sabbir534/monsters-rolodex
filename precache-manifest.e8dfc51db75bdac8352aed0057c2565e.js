2374faabc3d015b009b0634495dcae98c220b54d
sabbir534 <prince534534@gmail.com>
2019-08-05 21:43:48 +0600

    Updates

---
+++ precache-manifest.e8dfc51db75bdac8352aed0057c2565e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980d413b4431a199771a43e186f4aca4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "72743b207eb339b573c9",
+    "url": "/monsters-rolodex/static/css/main.091535d8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "72743b207eb339b573c9",
+    "url": "/monsters-rolodex/static/js/main.69b4ba5b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
